4118945429ed1c00cca35a4fe52efd1d599b9743
Ayush Khatri <aayushkhatri571@gmail.com>
2026-02-18 01:15:09 +0545

    first and request help page is implemented of user DashBoard

--- FixItNepalFrontend /src/Components/Dashboard/User-Dashboard/layout.jsx
+++ FixItNepalFrontend /src/Components/Dashboard/User-Dashboard/layout.jsx
@@ -4,7 +4,6 @@ export const Layout = () => {
   return (
     <div className="flex">
       <SideBar />
-      
       </div>
     
   );
---
+++ FixItNepalFrontend /src/Pages/Userdashboard.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Layout } from '../Components/Dashboard/User-Dashboard/layout'
+
+export const Userdashboard = () => {
+  return (
+    <div>
+        <Layout />
+    </div>
+  )
+}
