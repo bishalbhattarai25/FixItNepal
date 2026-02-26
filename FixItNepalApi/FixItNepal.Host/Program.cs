08d344096fe0e7acd0b3496b2d14964c08c37f85
bishalbhattarai25 <bishalbhattrai117@gmail.com>
2026-02-26 13:38:40 +0545

    added the cors policy to accept the frontend through deployment

--- FixItNepalApi/FixItNepal.Host/Program.cs
+++ FixItNepalApi/FixItNepal.Host/Program.cs
@@ -117,7 +117,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend", policy =>
     {
-        policy.WithOrigins("http://localhost:3000", "https://localhost:3000")
+        policy.WithOrigins("http://localhost:3000", "https://localhost:3000", "https://fix-it-nepal.vercel.app")
             .AllowAnyHeader()
             .AllowAnyMethod()
             .AllowCredentials();
