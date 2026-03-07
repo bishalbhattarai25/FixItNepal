88be79e988ceb3662bb627de308015c8228278aa
Ayush Khatri <aayushkhatri571@gmail.com>
2026-03-07 12:57:48 +0545

    fix: routing and add: compoundes

--- FixItNepalFrontend /src/Components/Navigation/NavData.jsx
+++ FixItNepalFrontend /src/Components/Navigation/NavData.jsx
@@ -2,6 +2,10 @@ export const NavData = [
     {title:"Home",path: "/" },
     {title:"User", path: '/userdashboard'},
     {title:"Service", path: '/servicecenter'},
+    {title:"MachinePage", path: '/machine'},
+    {title:"admin", path: '/admin'},
+
+    
 
     
 ]
