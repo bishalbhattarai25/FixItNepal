cbb0b5fbf76d4f3a6c1d071a75b987abb0830939
Ayush Khatri <aayushkhatri571@gmail.com>
2026-03-07 17:34:40 +0545

    added: formhandling is added

--- FixItNepalFrontend /src/Components/Login.jsx
+++ FixItNepalFrontend /src/Components/Login.jsx
@@ -1,26 +1,47 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export const Login = () => {
+  //for the navigation
+  const navigate = useNavigate();
+
+  //for form
+  const [loginForm, setLoginForm] = useState({
+    phonenumber: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    const { number, value } = e.target;
+
+    setLoginForm({ ...loginForm, [number]: value });
+  };
+
   return (
     <div className="min-h-screen flex items-center justify-center bg-zinc-100">
       <div className="shadow-2xl rounded-2xl p-8 w-[350px] bg-white">
-        
         {/* Logo / Title */}
         <div className="text-center mb-6">
           <span className="text-3xl font-extrabold text-orange-400">FIX</span>
           <span className="text-2xl font-bold">IT</span>
-          <span className="text-3xl font-extrabold text-green-400 "> Nepal</span>
+          <span className="text-3xl font-extrabold text-green-400 ">
+            {" "}
+            Nepal
+          </span>
           <p className="text-gray-500 mt-2">Login to your account</p>
         </div>
 
         {/* Form */}
         <form className="flex flex-col gap-4">
           <div>
-            <label className="font-semibold">Username</label>
+            <label className="font-semibold">Phone Number</label>
             <input
-              type="text"
+             name="phonenumber"
+              type="number"
               placeholder="Enter your username"
               className="w-full mt-1 p-2 rounded-xl bg-zinc-100 focus:outline-none focus:ring-2 focus:ring-black"
+              onChange={handleChange}
+              value={loginForm?.phonenumber}
             />
           </div>
 
@@ -30,6 +51,8 @@ export const Login = () => {
               type="password"
               placeholder="Enter your password"
               className="w-full mt-1 p-2 rounded-xl bg-zinc-100 focus:outline-none focus:ring-2 focus:ring-black"
+              onChange={handleChange}
+              value={loginForm?.password}
             />
           </div>
 
@@ -43,9 +66,15 @@ export const Login = () => {
 
         {/* Extra */}
         <p className="text-center text-sm text-gray-500 mt-4">
-          Don’t have an account? <span className="font-semibold cursor-pointer">Register</span>
+          Don’t have an account?{" "}
+          <span
+            className="font-semibold cursor-pointer"
+            onClick={() => navigate("/register")}
+          >
+            Register
+          </span>
         </p>
       </div>
     </div>
-  )
-}
+  );
+};
