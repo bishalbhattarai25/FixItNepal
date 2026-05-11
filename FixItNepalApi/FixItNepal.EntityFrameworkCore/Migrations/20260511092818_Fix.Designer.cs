1d216c695773069c71ab68a7105f4fcdc9ad0e52
bishalbhattarai25 <bishalbhattrai117@gmail.com>
2026-05-11 15:14:31 +0545

    fixed migration file

--- FixItNepalApi/FixItNepal.EntityFrameworkCore/Migrations/20260511092818_Fix.Designer.cs
+++ FixItNepalApi/FixItNepal.EntityFrameworkCore/Migrations/20260511092818_Fix.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using FixItNepal.EntityFrameworkCore.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace FixItNepal.EntityFrameworkCore.Migrations
 {
     [DbContext(typeof(ApiDbContext))]
-    partial class ApiDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260511092818_Fix")]
+    partial class Fix
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ FixItNepalApi/FixItNepal.EntityFrameworkCore/Migrations/20260511092818_Fix.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FixItNepal.EntityFrameworkCore.Migrations
+{
+    /// <inheritdoc />
+    public partial class Fix : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "RowVersion",
+                table: "AppServiceRequests",
+                type: "bytea",
+                rowVersion: true,
+                nullable: true,
+                oldClrType: typeof(byte[]),
+                oldType: "bytea",
+                oldRowVersion: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "RowVersion",
+                table: "AppServiceRequests",
+                type: "bytea",
+                rowVersion: true,
+                nullable: false,
+                defaultValue: new byte[0],
+                oldClrType: typeof(byte[]),
+                oldType: "bytea",
+                oldRowVersion: true,
+                oldNullable: true);
+        }
+    }
+}
